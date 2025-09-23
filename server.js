b552be9105df7e1b4319b9e5258e3aaa090f8103
tshegofatsoseane <tshegofatsoseane98@gmail.com>
2025-09-23 21:54:33 +0200

    chore:update server to use render’s dynamic port

--- server.js
+++ server.js
@@ -96,6 +96,6 @@ app.get('/', (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, '0.0.0.0', () => {
-    console.log(`Server running on port ${PORT}`);
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
